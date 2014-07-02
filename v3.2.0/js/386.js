0260be6b47b0ae95e92914cacb9f4165befc6dcd
Chris McKenzie <kristopolous@yahoo.com>
2014-07-01 20:15:43 -0700

    making grunt happy for v3.2

--- v3.2.0/js/386.js
+++ v3.2.0/js/386.js
@@ -1,14 +1,16 @@
-self._386 = self._386 || {};
+window._386 = window._386 || {};
 
-$(function(){
+$(function () {
+  'use strict';
   var character = { height: 20, width: 12.4 };
 
+/*
   function scrollLock() {
     var last = 0;
     $(window).bind('scroll', function(e) {
       var func, off = $(window).scrollTop();
 
-      console.log(off, last, off < last ? "up" : "down");
+      // console.log(off, last, off < last ? "up" : "down");
 
       // this determines whether the user is intending to go up or down.
       func = off < last ? "floor" : "ceil";
@@ -24,69 +26,71 @@ $(function(){
         Math[func](off / character.height) * character.height
       );
 
-    }); 
-  }  
+    });
+  }
+*/
 
-  function loading() {
+  function loading () {
 
-    if(_386.fastLoad) {
-      document.body.style.visibility='visible';
+    if (window._386.fastLoad) {
+      document.body.style.visibility = 'visible';
       return;
     }
 
-    var
-      onePass = _386.onePass,
-      speedFactor = 1 / (_386.speedFactor || 1) * 165000;
-      wrap = document.createElement('div'),
-      bar = wrap.appendChild(document.createElement('div')),
+    var onePass = window._386.onePass;
+    var speedFactor = 1 / (window._386.speedFactor || 1) * 165000;
+    var wrap = document.createElement('div');
+    var bar = wrap.appendChild(document.createElement('div'));
 
-      cursor = document.createElement('div'),
-      // If the user specified that the visibility is hidden, then we
-      // start at the first pass ... otherwise we just do the 
-      // cursor fly-by
-      pass = ($(document.body).css('visibility') == 'visible') ? 1 : 0,
-      height = $(window).height(),
-      width = $(window).width(),
+    var cursor = document.createElement('div');
 
-      // this makes the loading of the screen proportional to the real-estate of the window.
-      // it helps keep the cool sequence there while not making it waste too much time.
-      rounds = (height * width / speedFactor),
-      column = width, row = height - character.height;
-      
-    wrap.id = "wrap386";
-    bar.id = "bar386";
-    cursor.id = "cursor386";
+    // If the user specified that the visibility is hidden; then we
+    // start at the first pass ... otherwise we just do the
+    // cursor fly-by
+    var pass = ($(document.body).css('visibility') == 'visible') ? 1 : 0;
+    var height = $(window).height();
+    var width = $(window).width();
+
+    // this makes the loading of the screen proportional to the real-estate of the window.
+    // it helps keep the cool sequence there while not making it waste too much time.
+    var rounds = (height * width / speedFactor);
+    var column = width;
+    var row = height - character.height;
+
+    wrap.id = 'wrap386';
+    bar.id = 'bar386';
+    cursor.id = 'cursor386';
 
     cursor.innerHTML = bar.innerHTML = '&#9604;';
 
     // only inject the wrap if the pass is 0
-    if(pass === 0) {
+    if (pass === 0) {
       document.body.appendChild(wrap);
-      document.body.style.visibility='visible';
+      document.body.style.visibility = 'visible';
     } else {
       document.body.appendChild(cursor);
       rounds /= 2;
       character.height *= 4;
     }
 
-    var ival = setInterval(function(){
-      for(var m = 0; m < rounds; m++) {
+    var ival = setInterval(function () {
+      for (var m = 0; m < rounds; m++) {
         column -= character.width;
 
-        if(column <= 0) {
+        if (column <= 0) {
           column = width;
           row -= character.height;
         }
-        if(row <= 0) {
+        if (row <= 0) {
           pass++;
           row = height - character.height;
 
-          if(pass == 2) {
+          if (pass == 2) {
             document.body.removeChild(cursor);
             clearInterval(ival);
           } else {
             wrap.parentNode.removeChild(wrap);
-            if(onePass) {
+            if (onePass) {
               clearInterval(ival);
             } else {
               document.body.appendChild(cursor);
@@ -96,12 +100,12 @@ $(function(){
           }
         }
 
-        if(pass === 0) {
-          bar.style.width = column + "px";
-          wrap.style.height = row + "px";
+        if (pass === 0) {
+          bar.style.width = column + 'px';
+          wrap.style.height = row + 'px';
         } else {
-          cursor.style.right = column + "px";
-          cursor.style.bottom = row + "px";
+          cursor.style.right = column + 'px';
+          cursor.style.bottom = row + 'px';
         }
       }
     }, 1);
