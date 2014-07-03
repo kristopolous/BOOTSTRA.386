49daadaa0348935facdd03b573765fd4c7555cee
Chris McKenzie <kristopolous@yahoo.com>
2014-07-03 02:32:40 -0700

    updating deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+abort() {
+  echo "$2"
+  exit $1
+}
+
 [ -e ~/temp/docs ] && rm -r ~/temp/docs
 cp -r docs ~/temp
+
 git checkout gh-pages
-if [ $? -eq 0 ]; then
-  cp -r ~/temp/docs/* .
-  git commit -am "docs update"
-  git checkout master
-else
-  echo "Failure"
-fi
-# scp ./docs/assets/css/bootstrap.css 9ol.es:/usr/qaa.ath.cx/www/BOOTSTRA.386/assets/css
+[ $? -eq 0 ] || abort $? "Failed to checkout"
+
+cp -r ~/temp/docs/* .
+
+git commit -am "docs update"
+[ $? -eq 0 ] || abort $? "Failed to commit"
+
+git push
+[ $? -eq 0 ] || abort $? "Failed to push"
+
+git checkout master
