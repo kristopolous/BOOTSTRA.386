e4f853ca2701a62296641dd10f6f8e87636d510c
Chris McKenzie <kristopolous@yahoo.com>
2014-07-03 02:28:23 -0700

    Adding progressive loading to be default

--- docs/assets/js/bootstrap-386.js
+++ docs/assets/js/bootstrap-386.js
@@ -1,4 +1,4 @@
-function sequentialLoad() {
+$(function(){
   var 
     character = {
       height: 20,
@@ -6,35 +6,36 @@ function sequentialLoad() {
     },
     wrap = document.body.appendChild(document.createElement('div')),
     bar = wrap.appendChild(document.createElement('div')),
-    height = parseInt(window.innerHeight, 10),
-    width = parseInt(window.innerWidth, 10),
-    column = width, row = height - character.height;
+    height = $(window).height(),
+    width = $(window).width(),
 
+    // this makes the loading of the screen proportional to the real-estate of the window.
+    // it helps keep the cool sequence there while not making it waste too much time.
+    rounds = (height * width / 175000),
+    column = width, row = height - character.height;
   wrap.setAttribute('style', 'z-index:9999999;background:#000084;position:fixed;bottom:0;right:0;height:100%;width:100%');
   bar.setAttribute('style', 'color:#fff;font-weight:bold;float:right;background:#000084;height:20px;margin-top:-20px;width:100%');
 
+  
+  console.log(height * width);
   bar.innerHTML = '_';
-  document.body.style.visibility='visible';
-  var i = setInterval(function(){
-  //approx. 9600b
-    for(var m = 0; m < 4; m++) {
+  document.body.style.visibility = 'visible';
+  var ival = setInterval(function(){
+    //approx. 9600b
+    for(var m = 0; m < rounds; m++) {
       column -= character.width;
 
-      console.log(row, height);
       if(column <= 0) {
         column = width;
-        row -= character.height, 
+        row -= character.height;
       }
       if(row <= 0) {
         wrap.parentNode.removeChild(wrap);
-        clearInterval(i);
+        clearInterval(ival);
       }
       bar.style.width = column + "px";
       wrap.style.height = row + "px";
     }
   }, 1);
-}
-window.onload = function(){
-  sequentialLoad();
-}
+});
 
