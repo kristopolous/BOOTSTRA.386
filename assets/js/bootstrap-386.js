2fa73207861b6375c0911309d110b5abec9b3598
Chris McKenzie <kristopolous@yahoo.com>
2014-07-03 06:29:25 -0700

    docs update

--- assets/js/bootstrap-386.js
+++ assets/js/bootstrap-386.js
@@ -20,6 +20,7 @@ $(function(){
     rounds = (height * width / 165000),
     column = width, row = height - character.height;
     // TODO: externalize this
+    //
   wrap.setAttribute('style', 'z-index:9999999;background:#000084;position:fixed;bottom:0;right:0;height:100%;width:100%');
   bar.setAttribute('style', 'color:#fff;font-weight:bold;float:right;background:#000084;height:20px;margin-top:-20px;width:100%');
   cursor.setAttribute('style', 'z-index:9999999;color:#fff;font-weight:bold;position:fixed;bottom:0;right:0');
