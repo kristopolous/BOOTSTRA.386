8b996c2864949bd2c42072bd8cdc5ab3deb3355d
Chris McKenzie <kristopolous@yahoo.com>
2014-07-04 05:17:08 -0700

    Fixes #19 - adding a process for creating a distribution zip

--- Makefile
+++ Makefile
@@ -63,6 +63,7 @@ clean:
 #
 
 bootstrap: bootstrap-img bootstrap-css bootstrap-js
+	zip -r -9 bootstrap-latest.zip bootstrap
 
 
 #
--- convert-image.sh
+++ convert-image.sh
@@ -7,12 +7,12 @@
 
 ascii() {
   tmp=`mktemp --suffix=png`
-  convert $1 -remap img/ega16-palette.png -resize 70x $tmp
+  convert $1 -remap img-ref/ega16-palette.png -resize 70x $tmp
   img2txt -W 30 -x 12.4 -y 20 -f tga $1 | convert tga:- $1
   rm $tmp
 }
 
 color16() {
-  convert $1 -resize 120x - | convert - -filter box -dither FloydSteinberg -remap img/ega16-palette.png -resize 360x $1
+  convert $1 -resize 120x - | convert - -filter box -dither FloydSteinberg -remap img-ref/ega16-palette.png -resize 360x $1
 }
 color16 $1
