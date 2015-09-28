cdcc6a54fc40c7c2c8547d3e29e7e84304ea8aae
Chris McKenzie <kristopolous@yahoo.com>
2015-09-27 22:48:56 -0700

    making the 386 code somewhat follow the stupid rules
    lol, I can't believe that on the actual bootstrap code people
    have to put up with this petty picky shitty rules that some
    asshole come up with who has an ego way past the douchebag
    threshold.
    
    I am so so so glad I'm running my own fork of this with the
    "does it work in the browser? good. let's roll" coding standard.

--- v3.3.2/dist/js/bootstrap.js
+++ v3.3.2/dist/js/bootstrap.js
@@ -16,24 +16,25 @@ if (typeof jQuery === 'undefined') {
   }
 }(jQuery);
 
-self._386 = self._386 || {};
+window._386 = window._386 || {};
 
-$(function(){
+$(function () {
   'use strict';
   var character = { height: 20, width: 10 };
 
+  /*
   function scrollLock() {
     var last = 0;
     $(window).bind('scroll', function(e) {
       var func, off = $(window).scrollTop();
 
-      console.log(off, last, off < last ? "up" : "down");
+      //console.log(off, last, off < last ? "up" : "down");
 
       // this determines whether the user is intending to go up or down.
       func = off < last ? "floor" : "ceil";
 
       // make sure we don't run this from ourselves
-      if(off % character.height === 0) {
+      if (off % character.height === 0) {
         return;
       }
       last = off;
@@ -43,25 +44,26 @@ $(function(){
         Math[func](off / character.height) * character.height
       );
 
-    }); 
-  }  
+    });
+  }
+  */
 
-  function loading() {
+  function loading () {
 
-    if(_386.fastLoad) {
-      document.body.style.visibility='visible';
+    if (window._386.fastLoad) {
+      document.body.style.visibility = 'visible';
       return;
     }
 
     var
-      onePass = _386.onePass,
-      speedFactor = 1 / (_386.speedFactor || 1) * 165000,
+      onePass = window._386.onePass,
+      speedFactor = 1 / (window._386.speedFactor || 1) * 165000,
       wrap = document.createElement('div'),
       bar = wrap.appendChild(document.createElement('div')),
 
       cursor = document.createElement('div'),
       // If the user specified that the visibility is hidden, then we
-      // start at the first pass ... otherwise we just do the 
+      // start at the first pass ... otherwise we just do the
       // cursor fly-by
       pass = ($(document.body).css('visibility') == 'visible') ? 1 : 0,
       height = $(window).height(),
@@ -71,41 +73,41 @@ $(function(){
       // it helps keep the cool sequence there while not making it waste too much time.
       rounds = (height * width / speedFactor),
       column = width, row = height - character.height;
-      
-    wrap.id = "wrap386";
-    bar.id = "bar386";
-    cursor.id = "cursor386";
+
+    wrap.id = 'wrap386';
+    bar.id = 'bar386';
+    cursor.id = 'cursor386';
 
     cursor.innerHTML = bar.innerHTML = '&#9604;';
 
     // only inject the wrap if the pass is 0
-    if(pass === 0) {
+    if (pass === 0) {
       document.body.appendChild(wrap);
-      document.body.style.visibility='visible';
+      document.body.style.visibility = 'visible';
     } else {
       document.body.appendChild(cursor);
       rounds /= 2;
       character.height *= 4;
     }
 
-    var ival = setInterval(function(){
-      for(var m = 0; m < rounds; m++) {
+    var ival = setInterval(function () {
+      for (var m = 0; m < rounds; m++) {
         column -= character.width;
 
-        if(column <= 0) {
+        if (column <= 0) {
           column = width;
           row -= character.height;
         }
-        if(row <= 0) {
+        if (row <= 0) {
           pass++;
           row = height - character.height;
 
-          if(pass == 2) {
+          if (pass == 2) {
             document.body.removeChild(cursor);
             clearInterval(ival);
           } else {
             wrap.parentNode.removeChild(wrap);
-            if(onePass) {
+            if (onePass) {
               clearInterval(ival);
             } else {
               document.body.appendChild(cursor);
@@ -115,12 +117,12 @@ $(function(){
           }
         }
 
-        if(pass === 0) {
-          bar.style.width = column + "px";
-          wrap.style.height = row + "px";
+        if (pass === 0) {
+          bar.style.width = column + 'px';
+          wrap.style.height = row + 'px';
         } else {
-          cursor.style.right = column + "px";
-          cursor.style.bottom = row + "px";
+          cursor.style.right = column + 'px';
+          cursor.style.bottom = row + 'px';
         }
       }
     }, 1);
