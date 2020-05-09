c0890ac0bbea9d9549bdfcfdfa5fa8cc35f5e314
Chris McKenzie <kristopolous@yahoo.com>
2020-05-09 16:59:29 -0700

    a better deploy script? Ceci n'est pas une ... alright I don't know french ...

--- update-demo.sh
+++ update-demo.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
-cp v4.4.1/site/docs/4.4/demo.html site/
-cp v4.4.1/dist/css/bootstrap.css site/
-cd site
-git commit -am "updates"
-git push origin HEAD
+#!/bin/bash
+# I need to like inline sed some crap
+# ug why ... ... ug! Also zsh does better replacers
+cp v4.4.1/site/docs/4.4/demo.html v4.4.1/dist/{css/bootstrap.css,js/bootstrap.bundle.js} site/
+
+# Let's hope this doesn't break the planet ... all I need
+# to do is make this shit local. lol those 3 slashes, classic
+# exercis left up to the reader.
+sed -Ei s'/\/docs\/4.4\/dist\/(js|css)\///g' site/demo.html
+
+#git commit -am "updates"
+#git push origin HEAD
