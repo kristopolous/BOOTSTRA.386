ee5a315ed687368a5ce4edbc2bc54535aa649e88
Chris McKenzie <kristopolous@yahoo.com>
2020-05-09 21:37:08 -0700

    updates

--- update-demo.sh
+++ update-demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # I need to like inline sed some crap
 # ug why ... ... ug! Also zsh does better replacers
-cp v4.4.1/site/docs/4.4/demo.html v4.4.1/dist/{css/bootstrap.css,js/bootstrap.bundle.js} site/
+cp -v v4.4.1/site/docs/4.4/demo.html v4.4.1/dist/{css/bootstrap.css,js/bootstrap.bundle.js} site/
 
 # Let's hope this doesn't break the planet ... all I need
 # to do is make this shit local. lol those 3 slashes, classic
