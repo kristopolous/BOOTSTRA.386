5dad51359d609af72ce57289685d2242c3c7b200
Chris McKenzie <kristopolous@yahoo.com>
2020-05-15 19:28:39 -0700

    fixing a typo

--- update-demo.sh
+++ update-demo.sh
@@ -5,8 +5,8 @@ cp -v v4.4.1/site/docs/4.4/demo.html v4.4.1/dist/{css/bootstrap.css,js/bootstrap
 
 # Let's hope this doesn't break the planet ... all I need
 # to do is make this shit local. lol those 3 slashes, classic
-# exercis left up to the reader.
-sed -Ei s'/\/docs\/4.4\/dist\/(js|css)\///g' site/demo.html
+# exercise left up to the reader.
+sed -Ei s'/\/docs\/4.4\/dist\/(css|js)\///g' site/demo.html
 
 cd site
 git commit -am "updates"
