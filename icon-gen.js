8f8aaef1f80196aa0b73f31a924ec8d92c08c80d
Chris McKenzie <kristopolous@yahoo.com>
2020-05-25 16:04:04 -0700

    more work on the icon gen

--- icon-gen.js
+++ icon-gen.js
@@ -2,15 +2,23 @@
 const TextToSVG = require('text-to-svg');
 const textToSVG = TextToSVG.loadSync('fonts/Px437_IBM_EGA8.otf');
 const fs = require('fs');
+var list = ["// This file is auto-generated from icon-gen in the root directory"];
  
 [
-  ['[\u25A0]', 'checkbox-checked'],
-  ['[ ]', 'checkbox-unchecked'],
-  ['(\u2022)', 'radio-checked'],
-  ['( )', 'radio-unchecked']
+  ['[\u25A0]', 'checkbox-checked', 'custom-checkbox-indicator-icon-checked'],
+  ['[ ]', 'checkbox-unchecked', ''],
+  ['(\u2022)', 'radio-checked', ''],
+  ['( )', 'radio-unchecked', '']
 ].forEach(row => {
-  let [str, name] = row;
+  let [str, name, bsname] = row;
   const options = {x: 0, y: 0, fontSize: 18, anchor: 'top'}
   const svg = textToSVG.getSVG(str, options);
-  fs.writeFile(`${name}.svg`, svg, () => console.log(name + " written"));
+  fs.writeFileSync(`${name}.svg`, svg);
+  if(bsname) {
+    list.push(`$${bsname}: url('data:image/svg+xml,${svg}')`);
+  }
 });
+
+fs.writeFileSync(`v4.4.1/scss/_autogen.scss`, list.join('\n'));/*(, (e) => {
+  console.log("Auto gen written, content:\n" + list.join('\n'));
+});*/
