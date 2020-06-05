f20fd41b198636bd8e78c98d037071df9fc47d3e
Chris McKenzie <kristopolous@yahoo.com>
2020-06-04 23:02:41 -0700

    updating the deploy script to create the zip

--- deploy-v4.sh
+++ deploy-v4.sh
@@ -3,6 +3,15 @@
 # ug why ... ... ug! Also zsh does better replacers
 cp -v v4.4.1/site/docs/4.4/demo.html v4.4.1/dist/{css/bootstrap.css,js/bootstrap.bundle.js} site/
 
+# we're going to make this stuff we keep out of the repo
+build=build
+rm -r $build
+mkdir -p $build/css/fonts $build/js
+./update-icons.sh $build/css/fonts
+cp -puv v4.4.1/site/docs/4.4/dist/css/fonts/* $build/css/fonts
+cp -puv v4.4.1/site/docs/4.4/demo.html v4.4.1/dist/{css/bootstrap.css,js/bootstrap.bundle.js} $build
+zip -r bootstra.386-latest-v4.zip build
+
 # Let's hope this doesn't break the planet ... all I need
 # to do is make this shit local. lol those 3 slashes, classic
 # exercise left up to the reader.
