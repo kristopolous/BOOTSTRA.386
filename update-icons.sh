92f3d9a3649b7b251f898b6caa94212d97b74e7a
Chris McKenzie <kristopolous@yahoo.com>
2020-06-06 17:47:21 -0700

    using a variable for the font path for a cleaner distrib file

--- update-icons.sh
+++ update-icons.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-path=${1:-v4.4.1/site/docs/4.4/dist/css/fonts/ site/fonts/}
+path=${1:-v4.4.1/site/docs/4.4/dist/fonts/ site/fonts/}
 set -x 
 for i in $path; do
+  [ -d $i ] || mkdir -p $i
 	./icon-gen.js $i
 done
