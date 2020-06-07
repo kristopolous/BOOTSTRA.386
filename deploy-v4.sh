02bb26de3e4c8768d5d313eec15c6fdfb63b3bed
Chris McKenzie <kristopolous@yahoo.com>
2020-06-06 18:07:41 -0700

    updating the deploy system

--- deploy-v4.sh
+++ deploy-v4.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
-# I need to like inline sed some crap
-# ug why ... ... ug! Also zsh does better replacers
-cp -v v4.4.1/site/docs/4.4/demo.html v4.4.1/dist/{css/bootstrap.css,js/bootstrap.bundle.js} site/
 
-# we're going to make this stuff we keep out of the repo
-build=build
-rm -r $build
-mkdir -p $build/css/fonts $build/js
-./update-icons.sh $build/css/fonts
-cp -puv v4.4.1/site/docs/4.4/dist/css/fonts/* $build/css/fonts
-cp -puv v4.4.1/dist/css/bootstrap.css $build/css
-cp -puv v4.4.1/dist/js/bootstrap.bundle.js $build/js
-zip -r bootstra.386-latest-v4.zip build
+make_build() {
+  # we're going to make this stuff we keep out of the repo
+  build=build
+  rm -r $build
+  mkdir -p $build/{css,fonts,js}
+  ./update-icons.sh $build/fonts
+  for what in fonts css js; do
+    cp -puv v4.4.1/dist/$what/* $build/$what
+  done
 
-# Let's hope this doesn't break the planet ... all I need
-# to do is make this shit local. lol those 3 slashes, classic
-# exercise left up to the reader.
-sed -Ei s'/\/docs\/4.4\/dist\/(css|js)\///g' site/demo.html
+  [[ -e bootstra.386-latest-v4.zip ]] && rm bootstra.386-latest-v4.zip
 
-cd site
-git commit -am "updates"
-git push origin HEAD
+  zip -r bootstra.386-latest-v4.zip $build
+}
+
+deploy_site(){
+  # I need to like inline sed some crap
+  # ug why ... ... ug! Also zsh does better replacers
+  cp -v v4.4.1/site/docs/4.4/demo.html v4.4.1/dist/{css/bootstrap.css,js/bootstrap.bundle.js} site/
+
+  # Let's hope this doesn't break the planet ... all I need
+  # to do is make this shit local. lol those 3 slashes, classic
+  # exercise left up to the reader.
+  sed -Ei s'/\/docs\/4.4\/dist\/(css|js)\///g' site/demo.html
+
+  cd site
+  git commit -am "updates"
+  git push origin HEAD
+}
+
+todo=${1:-make_build deploy_site}
+for i in $todo; do
+  eval $i
+done
+
