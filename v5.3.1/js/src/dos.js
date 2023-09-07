d936c0587e01a254057ca03bc58b37cee3fb6bdf
Chris McKenzie <kristopolous@yahoo.com>
2023-09-06 21:19:29 -0700

    making the js linter shutup

--- v5.3.1/js/src/dos.js
+++ v5.3.1/js/src/dos.js
@@ -1,21 +1,21 @@
-window._386 = window._386 || {options: {}};
+let _386 = window._386 = window._386 || { options: {} }
 
-const height = 16;
+const height = 16
 var character = { height: height, width: 8 },
-    keyspace = 'hgdlaoybSru6x2vHsFvhoQ386';
+    keyspace = 'hgdlaoybSru6x2vHsFvhoQ386'
 
 _386.magicCursor = function() {
   if(!document.querySelector('.bootstra-cursor')) {
-    let b = document.body;
-    let d = b.appendChild(document.createElement('div'));
-    let h = height - 2;
+    let b = document.body
+    const d = b.appendChild(document.createElement('div'))
+    const h = height - 2
 
-    b.classList.add('bootstra-enable-cursor');
-    d.className = 'bootstra-cursor';
+    b.classList.add('bootstra-enable-cursor')
+    d.className = 'bootstra-cursor'
 
     b.onmousemove = function(e) {
-      d.style.left = Math.floor( e.pageX / character.width) * character.width + 'px';
-      d.style.top = Math.floor(e.pageY  / h) * h + 'px';
+      d.style.left = Math.floor( e.pageX / character.width) * character.width + 'px'
+      d.style.top = Math.floor(e.pageY  / h) * h + 'px'
     }
   }
 }
@@ -26,14 +26,14 @@ _386.scrollLock = function () {
   }
 
   if(_386.scrollLock.listener) {
-    window.removeEventListener(_386.scrollLock.listener);
-    _386.scrollLock.listener = false;
+    window.removeEventListener(_386.scrollLock.listener)
+    _386.scrollLock.listener = false
   } else {
     if(!('paddingStart' in _386.scrollLock)) {
-      _386.scrollLock.paddingStart = parseInt(document.body.style.paddingTop, 10) || 0;
+      _386.scrollLock.paddingStart = parseInt(document.body.style.paddingTop, 10) || 0
     }
-    window.addEventListener('scroll', ev, true);
-    _386.scrollLock.listener = ev;
+    window.addEventListener('scroll', ev, true)
+    _386.scrollLock.listener = ev
   }
 }
 
@@ -47,21 +47,21 @@ _386.centerNudger = function() {
     !row[1].dataset[keyspace + 'nudge'] &&
     row[1].textContent.replace(/\s+/, ' ').length % 2 == 1
   ).forEach(row => {
-    row.marginLeft = (parseInt(row[1].marginLeft) || 0) + character.width + 'px';
+    row.marginLeft = (parseInt(row[1].marginLeft) || 0) + character.width + 'px'
     // make sure we don't do this again.
-    row[1].dataset[keyspace + 'nudge'] = 1;
+    row[1].dataset[keyspace + 'nudge'] = 1
   });
 }
 
 
 $(function () {
-  'use strict';
+  'use strict'
 
   function loading () {
 
     if (window._386.fastLoad) {
-      document.body.style.visibility = 'visible';
-      return;
+      document.body.style.visibility = 'visible'
+      return
     }
 
     var
@@ -81,45 +81,45 @@ $(function () {
       // this makes the loading of the screen proportional to the real-estate of the window.
       // it helps keep the cool sequence there while not making it waste too much time.
       rounds = (height * width / speedFactor),
-      column = width, row = height - character.height;
+      column = width, row = height - character.height
 
-    wrap.id = 'wrap386';
-    bar.id = 'bar386';
-    cursor.id = 'cursor386';
+    wrap.id = 'wrap386;
+    bar.id = 'bar386'
+    cursor.id = 'cursor386'
 
-    cursor.innerHTML = bar.innerHTML = '&#9604;';
+    cursor.innerHTML = bar.innerHTML = '&#9604;'
 
     // only inject the wrap if the pass is 0
     if (pass === 0) {
-      document.body.appendChild(wrap);
-      document.body.style.visibility = 'visible';
+      document.body.appendChild(wrap)
+      document.body.style.visibility = 'visible'
     } else {
-      document.body.appendChild(cursor);
-      rounds /= 2;
-      character.height *= 4;
+      document.body.appendChild(cursor)
+      rounds /= 2
+      character.height *= 4
     }
 
     var ival = setInterval(function () {
       for (var m = 0; m < rounds; m++) {
-        column -= character.width;
+        column -= character.width
 
         if (column <= 0) {
           column = width;
-          row -= character.height;
+          row -= character.height
         }
         if (row <= 0) {
-          pass++;
-          row = height - character.height;
+          pass++
+          row = height - character.height
 
           if (pass == 2) {
-            document.body.removeChild(cursor);
+            document.body.removeChild(cursor)
             clearInterval(ival);
           } else {
-            wrap.parentNode.removeChild(wrap);
+            wrap.parentNode.removeChild(wrap)
             if (onePass) {
-              clearInterval(ival);
+              clearInterval(ival)
             } else {
-              document.body.appendChild(cursor);
+              document.body.appendChild(cursor)
               rounds /= 2;
               character.height *= 4;
             }
