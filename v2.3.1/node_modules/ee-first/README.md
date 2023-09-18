6a053b01b8111eab1736928e7632bf06e7ba4a27
Chris McKenzie <kristopolous@yahoo.com>
2023-09-18 08:12:14 -0700

    trying to get gh-pages script to not crash

--- v2.3.1/node_modules/ee-first/README.md
+++
@@ -1,5 +0,0 @@
-
-# EE First
-
-Get the first event in a set of event emitters and event pairs,
-then clean up after itself.
--- v2.3.1/node_modules/nopt/lib/debug.js
+++
@@ -1,4 +0,0 @@
-/* istanbul ignore next */
-module.exports = process.env.DEBUG_NOPT || process.env.NOPT_DEBUG
-  ? (...a) => console.error(...a)
-  : () => {}
